bc8e2495f554e1e41daa99467e0b7e692e52acd2
XuChao <xuchao_1213@163.com>
2019-07-22 14:05:03 +0800

    :sparkles: add middleware 'RequestNotNil' & test

---
+++ middleware/req_not_nill.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"errors"
+	"gitee.com/gen-iot/rpcx"
+	"reflect"
+)
+
+var errRequestNil = errors.New("request must not be nil")
+
+func RequestNotNil() rpcx.MiddlewareFunc {
+	return func(next rpcx.HandleFunc) rpcx.HandleFunc {
+		return func(ctx rpcx.Context) {
+			reqInterface := ctx.Request()
+			reqV := reflect.ValueOf(reqInterface)
+			k := reqV.Kind()
+			switch k {
+			case reflect.Invalid:
+				goto error
+			case reflect.Chan, reflect.Func, reflect.Map, reflect.Ptr, reflect.UnsafePointer, reflect.Interface, reflect.Slice:
+				if reqV.IsNil() {
+					goto error
+				}
+			}
+			goto next
+		error:
+			ctx.SetError(errRequestNil)
+			return
+		next:
+			next(ctx)
+		}
+	}
+}
