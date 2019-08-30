856fcf4fb5cf87618f201443f7d3a595e78e11ce
Suzhen <suzhen@homeinpocket.net>
2019-08-30 14:40:42 +0800

    :sparkles: close #9, validate support validateFlag

--- middleware/validate.go
+++ middleware/validate.go
@@ -6,37 +6,49 @@ import (
 	"log"
 )
 
-func Validate(v std.Validator) rpcx.MiddlewareFunc {
+type ValidateFlag uint8
+
+const (
+	ValidateNone ValidateFlag = 0x00
+	ValidateIn   ValidateFlag = 0x01
+	ValidateOut  ValidateFlag = 0x02
+)
+
+func Validate(flag ValidateFlag, v std.Validator) rpcx.MiddlewareFunc {
 	std.Assert(v != nil, "validator is nil")
 	return func(next rpcx.HandleFunc) rpcx.HandleFunc {
 		return func(ctx rpcx.Context) {
-			err := ctx.Error()
-			if err != nil {
-				log.Println("validate req abort ,caused by:", err)
-				return
-			}
-			if ctx.LocalFuncDesc()&rpcx.ReqHasData != 0 {
-				err = v.Validate(ctx.Request())
+			if flag&ValidateIn != 0 {
+				err := ctx.Error()
 				if err != nil {
-					log.Println("validate req err")
-					ctx.SetError(err)
+					log.Println("validate req abort ,caused by:", err)
 					return
+				}
+				if ctx.LocalFuncDesc()&rpcx.ReqHasData != 0 {
+					err = v.Validate(ctx.Request())
+					if err != nil {
+						log.Println("validate req err")
+						ctx.SetError(err)
+						return
+					}
 				}
 			}
 			//
 			next(ctx)
 			//
-			err = ctx.Error()
-			if err != nil {
-				log.Println("validate rsp abort ,caused by:", err)
-				return
-			}
-			if ctx.LocalFuncDesc()&rpcx.RspHasData != 0 {
-				err = v.Validate(ctx.Response())
+			if flag&ValidateOut != 0 {
+				err := ctx.Error()
 				if err != nil {
-					log.Println("validate rsp err")
-					ctx.SetError(err)
+					log.Println("validate rsp abort ,caused by:", err)
 					return
+				}
+				if ctx.LocalFuncDesc()&rpcx.RspHasData != 0 {
+					err = v.Validate(ctx.Response())
+					if err != nil {
+						log.Println("validate rsp err")
+						ctx.SetError(err)
+						return
+					}
 				}
 			}
 		}
