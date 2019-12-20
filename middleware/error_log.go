189bedfd03035a60fc65b3cf87c0b173255753f8
Suzhen <suzhen@homeinpocket.net>
2019-12-20 17:55:56 +0800

    :bug: fix duplicate error log

--- middleware/error_log.go
+++ middleware/error_log.go
@@ -3,21 +3,25 @@ package middleware
 import (
 	"github.com/gen-iot/log"
 	"github.com/gen-iot/rpcx/v2"
+	"github.com/gen-iot/std"
 )
 
 // logger , such as log.Error
 func ErrorLog(logger log.Logger) rpcx.MiddlewareFunc {
-	ctxErrLog := func(ctx rpcx.Context) {
+	std.Assert(logger != nil, "logger is nil")
+	ctxErrLog := func(ctx rpcx.Context, previousErr error) error {
 		method := ctx.Method()
-		if err := ctx.Error(); err != nil {
+		if err := ctx.Error(); err != nil && previousErr != err {
 			logger.Printf("Method=%s,Error=%v\n", method, err)
+			return err
 		}
+		return nil
 	}
 	return func(next rpcx.HandleFunc) rpcx.HandleFunc {
 		return func(ctx rpcx.Context) {
-			ctxErrLog(ctx)
+			err1 := ctxErrLog(ctx, nil)
 			next(ctx)
-			ctxErrLog(ctx)
+			_ = ctxErrLog(ctx, err1)
 		}
 	}
 }
